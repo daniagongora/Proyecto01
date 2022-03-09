c657041b5d123a74ae978158176b3bb10225cc4a
Manuel Jesús Casillas Olivier <100379271+Chivo0407@users.noreply.github.com>
2022-03-09 17:39:01 -0600

    Create proyecto01.py

---
+++ proyecto01/proyecto01.py
@@ -0,0 +1,133 @@
+import requests
+import json
+import csv
+from io import open
+
+"""
+Programa que se encarga de hacer lo que pide el proyecto 1
+"""
+
+"""
+ENTRADA
+"""
+
+class Proyecto01:
+	
+	"""
+	Método para las peticiones
+	"""
+	def peticiones(diccionario):
+		
+		#diccionario nuevo para almacenar las ciudades con su clima
+		diccionarioClima={} 
+	
+		#llave de la api (esta es la de mi cuenta, pueden probar con otra de preferencia)
+		llaveApi="87dd4d6b93bcf3872531c2fecaf51962"
+		url="http://api.openweathermap.org/data/2.5/weather?"
+
+		#por cada ciudad en el diccionario de lectura hace una petición 
+		for clave in diccionario.keys():
+			
+			peticionApi=url+"lat="+diccionario[clave][0]+"&"+"lon="+diccionario[clave][1]+"&units=metric&lang=es&appid="+llaveApi
+
+			peticion=requests.get(peticionApi)
+			
+			#guardar las peticiones en json para despues manejarlas, en el diccionario nuevo
+			if peticion.status_code!=404:
+				diccionarioClima[clave]=peticion.json() 
+				
+
+		return diccionarioClima
+
+	"""
+	Método para leer el csv para almacenar los datos en un diccionario(cache) el cual 
+	se podra consultar
+	"""
+	def lecturaCache():
+	
+		#el cache donde se almacenara la información
+		cache={}
+	
+		#con DictReader puedo abrir el csv e iterar para leer más facilmente en un diccionario
+		documento = csv.DictReader(open("entrada/dataset1.csv"))
+	
+		for row in documento:
+			#guardara las coordenadas de cada ciudad de origen en una lista
+			origenCoordenadas=[row["origin_latitude"],row["origin_longitude"]]
+			ciudadOrigen=row["origin"]
+
+			#guardara las coordenadas de cada ciudad de destino en una lista
+			destinoCoordenadas=[row["destination_latitude"],row["destination_longitude"]]
+			ciudadDestino=row["destination"]
+
+			#guardara con las claves de ciudades sus coordenadas
+			cache[ciudadOrigen]=origenCoordenadas
+			cache[ciudadDestino]=destinoCoordenadas
+
+	
+		return cache
+"""
+SALIDA
+"""
+
+	"""
+	Método que mostrará los datos del clima de la ciudad dada con su iata
+	la información la consultara con lo guardado en el diccionario de
+	peticiones
+	"""
+	
+	def salidaClima(peticiones,iata):
+
+		#almacena los datos del clima
+		datos=peticiones[iata]
+
+		#convierte en string los datos para poder mostrarlos
+		temperatura=str(datos["main"]["temp"])
+		humedad=str(datos["main"]["humidity"])
+		sensacion=str(datos["main"]["feels_like"])
+		presion=str(datos["main"]["pressure"])
+		
+	"""
+	Método que ordena a cada conjunto de datos del clima con su respectivo vuelo en el .csv
+	"""
+	def leerBoletos(diccionarioPeticion):
+
+		diccionario=diccionarioPeticion
+		dataset=open("entrada/dataset1.csv")
+		reader = csv.reader(dataset)
+		origen=["lista"]
+		destino=["lista"]
+		s=0
+		for row in reader:
+			origen.append(row[0])
+			destino.append(row[1])
+		
+		for i in origen[2:]:
+			for j in destino[2:]:
+				#almacena los datos del clima
+				datos=diccionario[i]
+				datos2=diccionario[j]
+				#convierte en string los datos para poder mostrarlos
+				temperatura=str(datos["main"]["temp"])
+				humedad=str(datos["main"]["humidity"])
+				sensacion=str(datos["main"]["feels_like"])
+				presion=str(datos["main"]["pressure"])
+
+				temperatura2=str(datos2["main"]["temp"])
+				humedad2=str(datos2["main"]["humidity"])
+				sensacion2=str(datos2["main"]["feels_like"])
+				presion2=str(datos2["main"]["pressure"])
+				s=s+1
+				x=str(s)
+				print("el número: "+x)
+				print(
+					i+"\n lugar: "+datos["name"]+"\n temperatura: "+temperatura+
+					"\n humedad: "+humedad+"\n descripcion: "+datos["weather"][0]["description"]+
+					"\n con sensación de: "+sensacion+"\n presion: "+presion
+				)
+
+				print(
+					j+"\n lugar: "+datos2["name"]+"\n temperatura: "+temperatura2+
+					"\n humedad: "+humedad2+"\n descripcion: "+datos2["weather"][0]["description"]+
+					"\n con sensación de: "+sensacion2+"\n presion: "+presion2
+				)
