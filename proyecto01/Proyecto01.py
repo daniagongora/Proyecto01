7eb5af85412e9779fc69174ba650fc92c371e1ec
dania gongora <dania1012@ciencias.unam.mx>
2022-03-12 12:57:16 -0600

    error test corregido, inclusión de un main

--- proyecto01/Proyecto01.py
+++ proyecto01/Proyecto01.py
@@ -3,81 +3,132 @@ import json
 import csv
 from io import open
 
-
-class Proyecto01:
-
+"""
+Clase que se encarga de hacer lo que pide el proyecto 1
+"""	
+	
+def peticiones(diccionario):
 	"""
-	Clase que se encarga de hacer lo que pide el proyecto 1
+	Método para las peticiones
 	"""	
+	diccionarioClima={} 
 	
-	def peticiones(diccionario):
-		"""
-		Método para las peticiones
-		"""	
-		diccionarioClima={} 
-	
-		llaveApi="87dd4d6b93bcf3872531c2fecaf51962"
-		url="http://api.openweathermap.org/data/2.5/weather?"
+	llaveApi="87dd4d6b93bcf3872531c2fecaf51962"
+	url="http://api.openweathermap.org/data/2.5/weather?"
 
-		for clave in diccionario.keys():
+	for clave in diccionario.keys():
 			
-			peticionApi=url+"lat="+diccionario[clave][0]+"&"+"lon="+diccionario[clave][1]+"&units=metric&lang=es&appid="+llaveApi
+		peticionApi=url+"lat="+diccionario[clave][0]+"&"+"lon="+diccionario[clave][1]+"&units=metric&lang=es&appid="+llaveApi
 
-			peticion=requests.get(peticionApi)
+		peticion=requests.get(peticionApi)
 			
-			if peticion.status_code!=404:
-				diccionarioClima[clave]=peticion.json() 
+		if peticion.status_code!=404:
+			diccionarioClima[clave]=peticion.json() 
 				
 
-		return diccionarioClima
+	return diccionarioClima
 
 	
-	def lecturaCache():
-		"""
-		Método para leer el csv para almacenar los datos en un diccionario(cache) el cual 
-		se podra consultar
-		"""
-		cache={}
+def lecturaCache():
+	"""
+	Método para leer el csv para almacenar los datos en un diccionario(cache) el cual 
+	se podra consultar
+	"""
+	cache={}
 
-		documento = csv.DictReader(open("entrada/dataset1.csv"))
+	documento = csv.DictReader(open("entrada/dataset1.csv"))
 	
-		for row in documento:
+	for row in documento:
 			
-			origenCoordenadas=[row["origin_latitude"],row["origin_longitude"]]
-			ciudadOrigen=row["origin"]
+		origenCoordenadas=[row["origin_latitude"],row["origin_longitude"]]
+		ciudadOrigen=row["origin"]
 
 			
-			destinoCoordenadas=[row["destination_latitude"],row["destination_longitude"]]
-			ciudadDestino=row["destination"]
+		destinoCoordenadas=[row["destination_latitude"],row["destination_longitude"]]
+		ciudadDestino=row["destination"]
 
 			
-			cache[ciudadOrigen]=origenCoordenadas
-			cache[ciudadDestino]=destinoCoordenadas
+		cache[ciudadOrigen]=origenCoordenadas
+		cache[ciudadDestino]=destinoCoordenadas
 
 	
-		return cache
+	return cache
 
 		
-	def salidaClima(peticiones,iata):
-		"""
-		Método que mostrara los datos del clima de la ciudad dada con su iata
-		la información la consultara con lo guardado en el diccionario de
-		peticiones
-		"""
+def salidaClima(peticiones,iata):
+	"""
+	Método que mostrara los datos del clima de la ciudad dada con su iata
+	la información la consultara con lo guardado en el diccionario de
+	peticiones
+	"""
 		
-		datos=peticiones[iata]
+	datosDelClima=peticiones[iata]
 
-		temperatura=str(datos["main"]["temp"])
-		humedad=str(datos["main"]["humidity"])
-		sensacion=str(datos["main"]["feels_like"])
-		presion=str(datos["main"]["pressure"])
+	temperatura=str(datosDelClima["main"]["temp"])
+	humedad=str(datosDelClima["main"]["humidity"])
+	sensacion=str(datosDelClima["main"]["feels_like"])
+	presion=str(datosDelClima["main"]["pressure"])
 		
-		string1=iata+"\n lugar: "+datos["name"]+"\n temperatura: "+temperatura
-		string2="\n humedad: "+humedad+"\n descripcion: "+datos["weather"][0]["description"]
-		string3="\n con sensación de: "+sensacion+"\n presion: "+presion
-		informacion=string1+string2+string3
+	datoTemperatura=iata+"\n lugar: "+datosDelClima["name"]+"\n temperatura: "+temperatura
+	datoHumedad="\n humedad: "+humedad+"\n descripcion: "+datosDelClima["weather"][0]["description"]
+	datoSensacion="\n con sensación de: "+sensacion+"\n presion: "+presion
+	informacion=datoTemperatura+datoHumedad+datoSensacion
 		
 			
-		print(informacion)
-		return informacion
+	return informacion
 	
+def leerBoletos(diccionarioPeticion):
+	"""
+	Método que muestra los datos del clima
+	de los 3000 boletos
+	"""
+
+	diccionario=diccionarioPeticion
+	dataset=open("entrada/dataset1.csv")
+	reader = csv.reader(dataset)
+	origen=["lista"]
+	destino=["lista"]
+		
+	for row in reader:
+		origen.append(row[0])
+		destino.append(row[1])
+		
+	for i in origen[2:]:
+			
+		for j in destino[2:]:
+				
+			climaOrigen=diccionario[i]
+			climaDestino=diccionario[j]
+				
+			temperatura=str(climaOrigen["main"]["temp"])
+			humedad=str(climaOrigen["main"]["humidity"])
+			sensacion=str(climaOrigen["main"]["feels_like"])
+			presion=str(climaOrigen["main"]["pressure"])
+
+			temperatura2=str(climaDestino["main"]["temp"])
+			humedad2=str(climaDestino["main"]["humidity"])
+			sensacion2=str(climaDestino["main"]["feels_like"])
+			presion2=str(climaDestino["main"]["pressure"])
+				
+			print(
+				i+"\n lugar: "+climaOrigen["name"]+"\n temperatura: "+temperatura+
+				"\n humedad: "+humedad+"\n descripcion: "+climaOrigen["weather"][0]["description"]+
+				"\n con sensación de: "+sensacion+"\n presion: "+presion
+				)
+
+			print(
+				j+"\n lugar: "+climaDestino["name"]+"\n temperatura: "+temperatura2+
+				"\n humedad: "+humedad2+"\n descripcion: "+climaDestino["weather"][0]["description"]+
+				"\n con sensación de: "+sensacion2+"\n presion: "+presion2
+				)
+
+def main():
+	"""
+	Ejecuta el proyecto completo
+	"""
+
+	leerBoletos(peticiones(lecturaCache()))
+	
+if __name__=="__main__":
+
+	main()
--- proyecto01/Test.py
+++ proyecto01/Test.py
@@ -1,5 +1,8 @@
-from Proyecto01 import *
+from Proyecto01 import salidaClima
+from Proyecto01 import peticiones
+from Proyecto01 import lecturaCache
 import requests
+import json
 
 class Test:
 	""" 
@@ -13,7 +16,7 @@ class Test:
 		"""
 		
 		diccionarioPrueba={"MEX":["123","456"]}
-		assert(Proyecto01.peticiones(diccionarioPrueba)!=200)
+		assert(peticiones(diccionarioPrueba)!=200)
 
 	
 	def pruebaLecturaCache():
@@ -23,17 +26,23 @@ class Test:
 		"""
 
 		diccionarioPrueba=["49.0128","2.55"]
-		assert(Proyecto01.lecturaCache()["CDG"]==diccionarioPrueba)
+		assert(lecturaCache()["CDG"]==diccionarioPrueba)
 
 	def pruebaSalidaClima():
-		       
-		#se comprobará que la salida imprima correctamente las palabras clave que debe imprimir.
+		"""
+		método para probar que salidaClima arroja los datos correctos
+		"""
+		llaveApi="87dd4d6b93bcf3872531c2fecaf51962"
+		url="http://api.openweathermap.org/data/2.5/weather?"
 
-                
-                diccionarioPrueba1 = Proyecto01.lecturaCache()
-                diccionarioPrueba2 = Proyecto01.peticiones(diccionarioPrueba1)
-                assert("lugar" in Proyecto01.salidaClima(diccionarioPrueba2, "MEX"))
-                assert("humedad"in Proyecto01.salidaClima(diccionarioPrueba2, "MEX"))
-                assert("temperatura" in Proyecto01.salidaClima(diccionarioPrueba2,"MEX"))
-                assert("sensación" in Proyecto01.salidaClima(diccionarioPrueba2,"MEX"))
+		peticionApi=url+"lat=25.7785"+"&"+"lon=-100.107"+"&units=metric&lang=es&appid="+llaveApi
+
+		peticion=requests.get(peticionApi)
+		diccionarioPrueba={"MTY":peticion.json()}
+
+		assert("temperatura" in salidaClima(diccionarioPrueba,"MTY"))
+		assert("humedad" in salidaClima(diccionarioPrueba,"MTY"))
+		assert("lugar" in salidaClima(diccionarioPrueba,"MTY"))
+		assert("sensación" in salidaClima(diccionarioPrueba,"MTY"))
+
 	
